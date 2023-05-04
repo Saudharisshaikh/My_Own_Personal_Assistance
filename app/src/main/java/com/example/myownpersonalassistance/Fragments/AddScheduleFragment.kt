aab97ecd94b815f23d806dc99e099e7ce0b91abc
Saudharisshaikh <51926159+Saudharisshaikh@users.noreply.github.com>
2023-05-05 00:41:15 +0500

    list done

--- app/src/main/java/com/example/myownpersonalassistance/Fragments/AddScheduleFragment.kt
+++ app/src/main/java/com/example/myownpersonalassistance/Fragments/AddScheduleFragment.kt
@@ -1,11 +1,13 @@
 package com.example.myownpersonalassistance.Fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myownpersonalassistance.Fragments.Entities.TitleEntity
 import com.example.myownpersonalassistance.R
+import com.example.myownpersonalassistance.Utils.Util
 import com.example.myownpersonalassistance.databinding.FragmentAddScheduleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -13,12 +15,10 @@ import com.example.myownpersonalassistance.databinding.FragmentAddScheduleBindin
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddScheduleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class AddScheduleFragment : Fragment() {
+
+   lateinit var list: ArrayList<TitleEntity>
 
     lateinit var fragmentAddScheduleBinding: FragmentAddScheduleBinding
 
@@ -33,7 +33,45 @@ class AddScheduleFragment : Fragment() {
         return fragmentAddScheduleBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        list = ArrayList<TitleEntity> ()
+
+        val titleEntityOne:TitleEntity = TitleEntity(1,"Office Meeting")
+        val titleEntityTwo:TitleEntity = TitleEntity(2,"Hangout")
+        val titleEntityThree:TitleEntity = TitleEntity(3,"Emergency Meeting")
+        val titleEntityFour :TitleEntity = TitleEntity(4,"Others")
+
+        list.add(titleEntityOne)
+        list.add(titleEntityTwo)
+        list.add(titleEntityThree)
+        list.add(titleEntityFour)
 
 
+        fragmentAddScheduleBinding.titleLayout.setOnClickListener {
+
+            Util.showListDialogGeneric(
+                context,
+                list,
+                R.string.list_title,
+                listener = object :Util.selectedListener{
+                    override fun onListItemSelected(item: Int) {
+
+                        val titles: TitleEntity = list.get(item)
+                        fragmentAddScheduleBinding.title.setText(titles.name)
+                    }
+
+                }
+            )
+
+
+        }
+    }
+
+
+    public fun onSelectItem(postion:Int){
+
+    }
 
 }
--- app/src/main/java/com/example/myownpersonalassistance/Utils/Util.kt
+++ app/src/main/java/com/example/myownpersonalassistance/Utils/Util.kt
@@ -1,6 +1,6 @@
 package com.example.myownpersonalassistance.Utils
 
-import android.R
+import com.example.myownpersonalassistance.R
 import android.app.Activity
 import android.content.Context
 import android.view.LayoutInflater
@@ -20,36 +20,45 @@ class Util {
         fun onListItemSelected(item:Int);
     }
 
-    fun <T> showListDialogGeneric(
-        context: Context?,
-        list: ArrayList<TitleEntity>?,
-        @StringRes titleId: Int,
-        listener: selectedListener
-    ): AlertDialog? {
-        if (list == null) return null
-        if (context is Activity) {
+    companion object {
 
-            if (context.isDestroyed) return null
-        }
-        val items = arrayOfNulls<String>(list.size)
-        for (i in list.indices) {
-            items[i] = list[i].name
-        }
-        val view: View = LayoutInflater.from(context)
-            .inflate(R.layout.dialoglist,null,false)
-        val lv = view.findViewById<ListView>(R.id.lv)
-        lv.adapter =
-            ArrayAdapter(context!!, R.layout.simple_list_item_1, items)
-        val tv: TextView = view.findViewById(R.id.tvTitle)
-        tv.setText(titleId)
-        val ad = AlertDialog.Builder(context)
-            .setView(view)
-            .show()
-        lv.onItemClickListener =
-            OnItemClickListener { parent: AdapterView<*>?, view1: View?, position: Int, id: Long ->
-                ad.dismiss()
-                listener.onListItemSelected(position)
+
+
+
+        fun  showListDialogGeneric(
+            context: Context?,
+            list: ArrayList<TitleEntity>?,
+            @StringRes titleId: Int,
+            listener: selectedListener
+        ): AlertDialog? {
+            if (list == null) return null
+            if (context is Activity) {
+
+                if (context.isDestroyed) return null
             }
-        return ad
+            val items = arrayOfNulls<String>(list.size)
+            for (i in list.indices) {
+                items[i] = list[i].name
+            }
+
+            val layoutInflater = LayoutInflater.from(context)
+            val view: View = layoutInflater.inflate(R.layout.dialog_list, null, false)
+            val lv = view.findViewById<ListView>(R.id.lv)
+            lv.adapter =
+                ArrayAdapter(context!!, android.R.layout.simple_list_item_1, items)
+            val tv: TextView = view.findViewById(R.id.tvTitle)
+            tv.setText(titleId)
+            val ad = AlertDialog.Builder(context)
+                .setView(view)
+                .show()
+            lv.onItemClickListener =
+                OnItemClickListener { parent: AdapterView<*>?, view1: View?, position: Int, id: Long ->
+                    ad.dismiss()
+                    listener.onListItemSelected(position)
+                }
+            return ad
+        }
+
+
     }
 }
